e9af7627998f4a8d30573abc907e1cd784eef3f6
Ivan Dolgolaptsev <ivan.dolgolaptsev@krainet.by>
2025-05-07 00:50:32 +0300

    [INIT] create git repository

---
+++ src/test/java/com/testproject/decloudtesttask/DeCloudTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.decloudtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeCloudTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
